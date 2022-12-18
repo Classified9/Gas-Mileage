195e8e5c4edb0e5904ccd99d10b74510d057366a
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2022-12-18 07:21:31 +0100

    Added program files

---
+++ CreditLimitCalculator/CreditLimitCalculator.java
@@ -0,0 +1,66 @@
+package CreditLimitCalculator;
+
+import java.util.Scanner;
+
+public class CreditLimitCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int accountNumber = 0;
+        int initialBalance = 0;
+        int newBalance = 0;
+        int charges = 0;
+        int credits = 0;
+        int creditLimit = 0;
+        int flagValue = 0;
+
+
+        System.out.print("Enter customer account number or Type -1 to exit: ");
+        accountNumber= scan.nextInt();
+        if (accountNumber == -1) {
+            flagValue = -1 ;
+        }
+        else {
+            System.out.print("Enter customer balance: ");
+            initialBalance = scan.nextInt();
+            System.out.print("Enter total items charged by the end of this months: ");
+            charges = scan.nextInt();
+            System.out.print("Enter total credits applied to the customer account this months: ");
+            credits = scan.nextInt();
+            System.out.print("Enter customer allowed credit limit: ");
+            creditLimit = scan.nextInt();
+        }
+
+
+        while (flagValue != -1){
+            newBalance = initialBalance + charges - credits ;
+            if (newBalance > creditLimit){
+                System.out.println("\nCredit limit exceeded\n");
+            }
+            else {
+                System.out.println("\nCustomer did not exceed limit.\n");
+            }
+            System.out.print("\nEnter customer account number or Type -1 to exit: ");
+            accountNumber= scan.nextInt();
+            if (accountNumber == -1) {
+                flagValue = -1 ;
+            }
+            else {
+                System.out.print("Enter customer balance: ");
+                initialBalance = scan.nextInt();
+                System.out.print("Enter total items charged by the end of this months: ");
+                charges = scan.nextInt();
+                System.out.print("Enter total credits applied to the customer account this months: ");
+                credits = scan.nextInt();
+                System.out.print("Enter customer allowed credit limit: ");
+                creditLimit = scan.nextInt();
+            }
+        }
+
+        if (flagValue == -1){
+            System.out.println("\nThank you for coming!\n");
+        }
+
+
+
+    }
+}
